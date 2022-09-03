cdc934bbe24af2172881c4d9bf77394468a879e1
Dante Roldan <danteroldan72@gmail.com>
2022-09-03 00:25:02 -0300

    cambios

--- ejercicioNuev.js
+++ ejercicioNuev.js
@@ -7,6 +7,13 @@
  function saludar(color){
 document.querySelector('input').value = "El color es " + color;
 document.querySelector('.content').style.background = color;
+document.querySelector('.input').style.background = color;
+document.querySelector('.input').style.color = "white";
+
+
+if(color == "White" || color == "Yellow"){
+    document.querySelector('.input').style.color = "black";
+}
  }
 
  for(i=0; i < botones.length; i++){
